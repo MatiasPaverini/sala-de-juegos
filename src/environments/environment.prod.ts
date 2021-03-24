fc35322033cbc77775cf53d9b2dde70177118f0c
Matias Paverini <matias.paverini@tsoftlatam.com>
2021-03-24 13:25:39 -0300

    Sprint 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB8SSC3NZ2xGxij5E1PxhcCrKzQv9sH69s",
+    authDomain: "tp-sala-de-juegos-aa55d.firebaseapp.com",
+    databaseURL: "https://tp-sala-de-juegos-aa55d.firebaseio.com",
+    projectId: "tp-sala-de-juegos-aa55d",
+    storageBucket: "tp-sala-de-juegos-aa55d.appspot.com",
+    messagingSenderId: "6465975563",
+    appId: "1:6465975563:web:47a0efcf9b34b7034d5e0e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB8SSC3NZ2xGxij5E1PxhcCrKzQv9sH69s",
+    authDomain: "tp-sala-de-juegos-aa55d.firebaseapp.com",
+    databaseURL: "https://tp-sala-de-juegos-aa55d.firebaseio.com",
+    projectId: "tp-sala-de-juegos-aa55d",
+    storageBucket: "tp-sala-de-juegos-aa55d.appspot.com",
+    messagingSenderId: "6465975563",
+    appId: "1:6465975563:web:47a0efcf9b34b7034d5e0e"
+  }
 };
 
 /*
